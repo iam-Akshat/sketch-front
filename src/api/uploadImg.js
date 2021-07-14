203729903c4a9a8544df8e04b2aafe33df7d406d
Akshat Sethi <masterakshata@gmail.com>
2021-07-15 03:54:35 +0530

    Move backend to URL to constants.js

--- src/api/uploadImg.js
+++ src/api/uploadImg.js
@@ -1,12 +1,13 @@
+import constants from '../config/constants';
+
 const uploadImg = async (imgBlob) => {
   const myFile = new File([imgBlob], 'image.png', {
     type: imgBlob.type,
   });
-
   const formData = new FormData();
   formData.append('image', myFile);
 
-  const data = await fetch('http://localhost:3001/convert', {
+  const data = await fetch(`${constants.BACKEND_URL}/convert`, {
     method: 'POST',
     body: formData,
   });
